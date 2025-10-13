46f1f47c845758b6dd00b4578b355ab2a61822c0
Shagor-Mia <infinityshagor18557@gmail.com>
2025-10-13 14:57:30 +0600

    milestone 9 start and module47 start

---
+++ module47/src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Root from "./layouts/Root.jsx";
+import Home from "./components/Home.jsx";
+
+let router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
